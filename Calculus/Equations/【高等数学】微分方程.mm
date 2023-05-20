004394edae8e7b356ead1f7e1c706e0017d2616c
Eland Woo <gongzi_1076@163.com>
2023-05-20 22:12:14 +0800

    Update Equations.

--- Calculus/Equations/【高等数学】微分方程.mm
+++ Calculus/Equations/【高等数学】微分方程.mm
@@ -2,7 +2,17 @@
 <map>
   <node ID="root" TEXT="【高等数学】微分方程">
     <node TEXT="微分方程的概念" ID="o3z2Jxerk1" _mubu_text="%3Cspan%3E%E5%BE%AE%E5%88%86%E6%96%B9%E7%A8%8B%E7%9A%84%E6%A6%82%E5%BF%B5%3C/span%3E" STYLE="bubble" POSITION="right">
-      <node TEXT="" ID="pIseFHBwNw" _mubu_text="" STYLE="fork"/>
+      <node TEXT="微分方程：含导数（或微分）的方程；" ID="IdDzyfx1U4" _mubu_text="%3Cspan%3E%E5%BE%AE%E5%88%86%E6%96%B9%E7%A8%8B%EF%BC%9A%E5%90%AB%E5%AF%BC%E6%95%B0%EF%BC%88%E6%88%96%E5%BE%AE%E5%88%86%EF%BC%89%E7%9A%84%E6%96%B9%E7%A8%8B%EF%BC%9B%3C/span%3E" STYLE="fork"/>
+      <node TEXT="常微分方程：未知函数是一元的方程（只含x,y）；" ID="LjkdAgif3X" _mubu_text="%3Cspan%3E%E5%B8%B8%E5%BE%AE%E5%88%86%E6%96%B9%E7%A8%8B%EF%BC%9A%E6%9C%AA%E7%9F%A5%E5%87%BD%E6%95%B0%E6%98%AF%E4%B8%80%E5%85%83%E7%9A%84%E6%96%B9%E7%A8%8B%EF%BC%88%E5%8F%AA%E5%90%ABx,y%EF%BC%89%EF%BC%9B%3C/span%3E" STYLE="fork"/>
+      <node TEXT="微分方程的阶：未知函数最高阶导数的阶数；" ID="3syLaeIHeg" _mubu_text="%3Cspan%3E%E5%BE%AE%E5%88%86%E6%96%B9%E7%A8%8B%E7%9A%84%E9%98%B6%EF%BC%9A%E6%9C%AA%E7%9F%A5%E5%87%BD%E6%95%B0%E6%9C%80%E9%AB%98%E9%98%B6%E5%AF%BC%E6%95%B0%E7%9A%84%E9%98%B6%E6%95%B0%EF%BC%9B%3C/span%3E" STYLE="fork"/>
+      <node TEXT="微分方程的解：使得方程恒等的函数，可以进一步研究其性质；" ID="JGJ92PX9HQ" _mubu_text="%3Cspan%3E%E5%BE%AE%E5%88%86%E6%96%B9%E7%A8%8B%E7%9A%84%E8%A7%A3%EF%BC%9A%E4%BD%BF%E5%BE%97%E6%96%B9%E7%A8%8B%E6%81%92%E7%AD%89%E7%9A%84%E5%87%BD%E6%95%B0%EF%BC%8C%3C/span%3E%3Cspan%20class=%22bold%22%3E%E5%8F%AF%E4%BB%A5%E8%BF%9B%E4%B8%80%E6%AD%A5%E7%A0%94%E7%A9%B6%E5%85%B6%E6%80%A7%E8%B4%A8%3C/span%3E%3Cspan%3E%EF%BC%9B%3C/span%3E" STYLE="fork"/>
+      <node TEXT="微分方程的通解：独立常数个数与方程阶数相同的解；(与全部解不同)" ID="Po1UjwfU1a" _mubu_text="%3Cspan%20class=%22bold%22%3E%E5%BE%AE%E5%88%86%E6%96%B9%E7%A8%8B%E7%9A%84%E9%80%9A%E8%A7%A3%EF%BC%9A%E7%8B%AC%E7%AB%8B%E5%B8%B8%E6%95%B0%E4%B8%AA%E6%95%B0%E4%B8%8E%E6%96%B9%E7%A8%8B%E9%98%B6%E6%95%B0%E7%9B%B8%E5%90%8C%E7%9A%84%E8%A7%A3%EF%BC%9B(%E4%B8%8E%E5%85%A8%E9%83%A8%E8%A7%A3%E4%B8%8D%E5%90%8C)%3C/span%3E" STYLE="fork">
+        <node TEXT="如何证明：先代入证明是方程的解，再说明独立常数个数等于阶数。" ID="zdQyaEhRxh" _mubu_text="%3Cspan%3E%E5%A6%82%E4%BD%95%E8%AF%81%E6%98%8E%EF%BC%9A%E5%85%88%E4%BB%A3%E5%85%A5%E8%AF%81%E6%98%8E%E6%98%AF%E6%96%B9%E7%A8%8B%E7%9A%84%E8%A7%A3%EF%BC%8C%E5%86%8D%E8%AF%B4%E6%98%8E%E7%8B%AC%E7%AB%8B%E5%B8%B8%E6%95%B0%E4%B8%AA%E6%95%B0%E7%AD%89%E4%BA%8E%E9%98%B6%E6%95%B0%E3%80%82%3C/span%3E" STYLE="fork"/>
+      </node>
+      <node TEXT="初始条件与特解" ID="2rsT2udRju" _mubu_text="%3Cspan%3E%E5%88%9D%E5%A7%8B%E6%9D%A1%E4%BB%B6%E4%B8%8E%E7%89%B9%E8%A7%A3%3C/span%3E" STYLE="fork"/>
+      <node TEXT="线性与非线性微分方程" ID="KDrimrKW0I" _mubu_text="%3Cspan%3E%E7%BA%BF%E6%80%A7%E4%B8%8E%E9%9D%9E%E7%BA%BF%E6%80%A7%E5%BE%AE%E5%88%86%E6%96%B9%E7%A8%8B%3C/span%3E" STYLE="fork">
+        <node TEXT="" ID="WlB8ZzstiY" _mubu_text="" STYLE="fork"/>
+      </node>
     </node>
     <node TEXT="一阶微分方程" ID="ZyL9UKv2l0" _mubu_text="%3Cspan%3E%E4%B8%80%E9%98%B6%E5%BE%AE%E5%88%86%E6%96%B9%E7%A8%8B%3C/span%3E" STYLE="bubble" POSITION="right">
       <node TEXT="变量可分离型" ID="4iScU5z9WS" _mubu_text="%3Cspan%3E%E5%8F%98%E9%87%8F%E5%8F%AF%E5%88%86%E7%A6%BB%E5%9E%8B%3C/span%3E" STYLE="fork">
@@ -19,7 +29,7 @@
           <node TEXT="\frac{dx}{dy} = f(\frac{x}{y}) = &gt; \frac{du}{f(u) - u} = \frac{dy}{y}" ID="bj6N9JqVfM" _mubu_text="%3Cspan%20class=%22%20formula%22%20data-raw=%22%255Cfrac%257Bdx%257D%257Bdy%257D%2520%253D%2520f(%255Cfrac%257Bx%257D%257By%257D)%22%20contenteditable=%22false%22%3E%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%3C/span%3E%3Cspan%3E%20=%20&amp;gt;%20%3C/span%3E%3Cspan%20class=%22%20formula%22%20data-raw=%22%255Cfrac%257Bdu%257D%257Bf(u)%2520-%2520u%257D%2520%253D%2520%255Cfrac%257Bdy%257D%257By%257D%22%20contenteditable=%22false%22%3E%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%3C/span%3E" STYLE="fork"/>
         </node>
       </node>
-      <node TEXT="一阶线性型掌握推导过程，常考" ID="05kEEV0HCO" _mubu_text="%3Cspan%3E%E4%B8%80%E9%98%B6%E7%BA%BF%E6%80%A7%E5%9E%8B%3C/span%3E%3Cspan%20class=%22%20text-color-red%22%3E%E6%8E%8C%E6%8F%A1%E6%8E%A8%E5%AF%BC%E8%BF%87%E7%A8%8B%EF%BC%8C%E5%B8%B8%E8%80%83%3C/span%3E" STYLE="fork">
+      <node TEXT="一阶线性型掌握推导过程，常考" ID="05kEEV0HCO" _mubu_text="%3Cspan%20class=%22bold%22%3E%E4%B8%80%E9%98%B6%E7%BA%BF%E6%80%A7%E5%9E%8B%3C/span%3E%3Cspan%20class=%22%20text-color-red%22%3E%E6%8E%8C%E6%8F%A1%E6%8E%A8%E5%AF%BC%E8%BF%87%E7%A8%8B%EF%BC%8C%E5%B8%B8%E8%80%83%3C/span%3E" STYLE="fork">
         <node TEXT="y&apos; = p(x)y = q(x)(x,y可以调换)" ID="i9aBxlP03O" _mubu_text="%3Cspan%20class=%22%20formula%22%20data-raw=%22y&apos;%2520%253D%2520p(x)y%2520%253D%2520q(x)%22%20contenteditable=%22false%22%3E%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%3C/span%3E%3Cspan%20class=%22bold%20text-color-red%22%3E(x,y%E5%8F%AF%E4%BB%A5%E8%B0%83%E6%8D%A2)%3C/span%3E" STYLE="fork">
           <node TEXT="齐次型q(x)=0 =&gt; y = Ce^{-\int{p(x)dx}}" ID="qds1Cj5uwA" _mubu_text="%3Cspan%3E%E9%BD%90%E6%AC%A1%E5%9E%8B%3C/span%3E%3Cspan%20class=%22%20formula%22%20data-raw=%22q(x)%253D0%22%20contenteditable=%22false%22%3E%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%3C/span%3E%3Cspan%3E%20=&amp;gt;%20%3C/span%3E%3Cspan%20class=%22%20formula%22%20data-raw=%22y%2520%253D%2520Ce%255E%257B-%255Cint%257Bp(x)dx%257D%257D%22%20contenteditable=%22false%22%3E%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%3C/span%3E" STYLE="fork"/>
           <node TEXT="非齐次型q(x)\neq0=&gt;y = e^{-\int{p(x)dx}}[\int{e^{\int{p(x)dx}}q(x)dx} + C]" ID="gIcc7JeWGe" _mubu_text="%3Cspan%3E%E9%9D%9E%E9%BD%90%E6%AC%A1%E5%9E%8B%3C/span%3E%3Cspan%20class=%22%20formula%22%20data-raw=%22q(x)%255Cneq0%22%20contenteditable=%22false%22%3E%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%3C/span%3E%3Cspan%3E=&amp;gt;%3C/span%3E%3Cspan%20class=%22%20formula%22%20data-raw=%22y%2520%253D%2520e%255E%257B-%255Cint%257Bp(x)dx%257D%257D%255B%255Cint%257Be%255E%257B%255Cint%257Bp(x)dx%257D%257Dq(x)dx%257D%2520%252B%2520C%255D%22%20contenteditable=%22false%22%3E%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%3C/span%3E" STYLE="fork">
@@ -39,26 +49,7 @@
         <node TEXT="" ID="uxIkT8QNnw" _mubu_text="" STYLE="fork"/>
       </node>
     </node>
-    <node TEXT="高阶线性微分方程" ID="iR7ZpyP4zG" _mubu_text="%3Cspan%3E%E9%AB%98%E9%98%B6%E7%BA%BF%E6%80%A7%E5%BE%AE%E5%88%86%E6%96%B9%E7%A8%8B%3C/span%3E" STYLE="bubble" POSITION="right">
-      <node TEXT="常系数线性微分方程 y&apos;&apos; = py&apos; + qy = f(x)" ID="l3BVhyXarz" _mubu_text="%3Cspan%20class=%22bold%22%3E%E5%B8%B8%E7%B3%BB%E6%95%B0%E7%BA%BF%E6%80%A7%E5%BE%AE%E5%88%86%E6%96%B9%E7%A8%8B%3C/span%3E%3Cspan%3E%20%3C/span%3E%3Cspan%20class=%22%20formula%22%20data-raw=%22y&apos;&apos;%2520%253D%2520py&apos;%2520%252B%2520qy%2520%253D%2520f(x)%22%20contenteditable=%22false%22%3E%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%3C/span%3E" STYLE="fork">
-        <node TEXT="二阶齐次必考" ID="Bq9MSVuK1a" _mubu_text="%3Cspan%20class=%22bold%22%3E%E4%BA%8C%E9%98%B6%E9%BD%90%E6%AC%A1%3C/span%3E%3Cspan%20class=%22bold%20text-color-red%22%3E%E5%BF%85%E8%80%83%3C/span%3E" STYLE="fork">
-          <node TEXT="" ID="GLttXia56r" _mubu_text="" STYLE="fork"/>
-        </node>
-        <node TEXT="n阶齐次最多三阶" ID="CXqosuukip" _mubu_text="%3Cspan%20class=%22%20formula%22%20data-raw=%22n%22%20contenteditable=%22false%22%3E%E2%80%8B%3C/span%3E%3Cspan%3E%E9%98%B6%E9%BD%90%E6%AC%A1%3C/span%3E%3Cspan%20class=%22%20text-color-red%22%3E%E6%9C%80%E5%A4%9A%E4%B8%89%E9%98%B6%3C/span%3E" STYLE="fork">
-          <node TEXT="" ID="JwOxQxuJ3M" _mubu_text="" STYLE="fork"/>
-        </node>
-        <node TEXT="二阶非齐次" ID="NaoXEzGDvk" _mubu_text="%3Cspan%3E%E4%BA%8C%E9%98%B6%E9%9D%9E%E9%BD%90%E6%AC%A1%3C/span%3E" STYLE="fork">
-          <node TEXT="类型一" ID="DyabU8GAvC" _mubu_text="%3Cspan%3E%E7%B1%BB%E5%9E%8B%E4%B8%80%3C/span%3E" STYLE="fork">
-            <node TEXT="" ID="mGi6cvYVCb" _mubu_text="" STYLE="fork"/>
-          </node>
-          <node TEXT="类型二" ID="zfVmDFG35a" _mubu_text="%3Cspan%3E%E7%B1%BB%E5%9E%8B%E4%BA%8C%3C/span%3E" STYLE="fork">
-            <node TEXT="" ID="AZsbmAAw3N" _mubu_text="" STYLE="fork"/>
-          </node>
-          <node TEXT="例题" ID="hGLNRNhn4v" _mubu_text="%3Cspan%3E%E4%BE%8B%E9%A2%98%3C/span%3E" STYLE="fork">
-            <node TEXT="" ID="nElnNGZpAC" _mubu_text="" STYLE="fork"/>
-          </node>
-        </node>
-      </node>
+    <node TEXT="高阶线性微分方程" ID="iR7ZpyP4zG" _mubu_text="%3Cspan%3E%E9%AB%98%E9%98%B6%E7%BA%BF%E6%80%A7%E5%BE%AE%E5%88%86%E6%96%B9%E7%A8%8B%3C/span%3E" STYLE="bubble" POSITION="left">
       <node TEXT="解的结构" ID="Tlyuz0s6vY" _mubu_text="%3Cspan%3E%E8%A7%A3%E7%9A%84%E7%BB%93%E6%9E%84%3C/span%3E" STYLE="fork">
         <node TEXT="齐次的通解y(x) = C_1y_1(x) + C_2y_2(x)，y_1(x)和y_2(x)线性无关" ID="9fjXxgIUIG" _mubu_text="%3Cspan%3E%E9%BD%90%E6%AC%A1%E7%9A%84%E9%80%9A%E8%A7%A3%3C/span%3E%3Cspan%20class=%22%20formula%22%20data-raw=%22y(x)%2520%253D%2520C_1y_1(x)%2520%252B%2520C_2y_2(x)%25EF%25BC%258Cy_1(x)%25E5%2592%258Cy_2(x)%25E7%25BA%25BF%25E6%2580%25A7%25E6%2597%25A0%25E5%2585%25B3%22%20contenteditable=%22false%22%3E%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%3C/span%3E" STYLE="fork">
           <node TEXT="" ID="pvL5oIBDC1" _mubu_text="" STYLE="fork"/>
@@ -66,16 +57,39 @@
         <node TEXT="非齐次的通解y(x) + y^*(x)" ID="wNhgpkTbpA" _mubu_text="%3Cspan%3E%E9%9D%9E%E9%BD%90%E6%AC%A1%E7%9A%84%E9%80%9A%E8%A7%A3%3C/span%3E%3Cspan%20class=%22%20formula%22%20data-raw=%22y(x)%2520%252B%2520y%255E*(x)%22%20contenteditable=%22false%22%3E%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%3C/span%3E" STYLE="fork">
           <node TEXT="" ID="Z6LiMZnjLL" _mubu_text="" STYLE="fork"/>
         </node>
-        <node TEXT="解的叠加原理" ID="4PM9PaB94M" _mubu_text="%3Cspan%3E%E8%A7%A3%E7%9A%84%E5%8F%A0%E5%8A%A0%E5%8E%9F%E7%90%86%3C/span%3E" STYLE="fork"/>
+        <node TEXT="解的叠加原理" ID="4PM9PaB94M" _mubu_text="%3Cspan%3E%E8%A7%A3%E7%9A%84%E5%8F%A0%E5%8A%A0%E5%8E%9F%E7%90%86%3C/span%3E" STYLE="fork">
+          <node TEXT="" ID="nAgPu0Tk8k" _mubu_text="" STYLE="fork"/>
+        </node>
         <node TEXT="例题" ID="JaM7Br6CkB" _mubu_text="%3Cspan%3E%E4%BE%8B%E9%A2%98%3C/span%3E" STYLE="fork">
           <node TEXT="" ID="DyFuGooNsz" _mubu_text="" STYLE="fork"/>
         </node>
       </node>
+      <node TEXT="常系数线性微分方程 y&apos;&apos; = py&apos; + qy = f(x)" ID="l3BVhyXarz" _mubu_text="%3Cspan%20class=%22bold%22%3E%E5%B8%B8%E7%B3%BB%E6%95%B0%E7%BA%BF%E6%80%A7%E5%BE%AE%E5%88%86%E6%96%B9%E7%A8%8B%3C/span%3E%3Cspan%3E%20%3C/span%3E%3Cspan%20class=%22%20formula%22%20data-raw=%22y&apos;&apos;%2520%253D%2520py&apos;%2520%252B%2520qy%2520%253D%2520f(x)%22%20contenteditable=%22false%22%3E%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%3C/span%3E" STYLE="fork">
+        <node TEXT="n阶齐次的解和通解最多三阶" ID="CXqosuukip" _mubu_text="%3Cspan%20class=%22%20formula%22%20data-raw=%22n%22%20contenteditable=%22false%22%3E%E2%80%8B%3C/span%3E%3Cspan%3E%E9%98%B6%E9%BD%90%E6%AC%A1%E7%9A%84%E8%A7%A3%E5%92%8C%E9%80%9A%E8%A7%A3%3C/span%3E%3Cspan%20class=%22%20text-color-red%22%3E%E6%9C%80%E5%A4%9A%E4%B8%89%E9%98%B6%3C/span%3E" STYLE="fork">
+          <node TEXT="在“已知方程的解反求方程”的题目中，需要逆用" ID="JwOxQxuJ3M" _mubu_text="%3Cspan%20class=%22bold%22%3E%E5%9C%A8%E2%80%9C%E5%B7%B2%E7%9F%A5%E6%96%B9%E7%A8%8B%E7%9A%84%E8%A7%A3%E5%8F%8D%E6%B1%82%E6%96%B9%E7%A8%8B%E2%80%9D%E7%9A%84%E9%A2%98%E7%9B%AE%E4%B8%AD%EF%BC%8C%E9%9C%80%E8%A6%81%E9%80%86%E7%94%A8%3C/span%3E" STYLE="fork"/>
+        </node>
+        <node TEXT="二阶齐次的通解必考" ID="Bq9MSVuK1a" _mubu_text="%3Cspan%20class=%22bold%22%3E%E4%BA%8C%E9%98%B6%E9%BD%90%E6%AC%A1%E7%9A%84%E9%80%9A%E8%A7%A3%3C/span%3E%3Cspan%20class=%22bold%20text-color-red%22%3E%E5%BF%85%E8%80%83%3C/span%3E" STYLE="fork">
+          <node TEXT="" ID="GLttXia56r" _mubu_text="" STYLE="fork"/>
+        </node>
+        <node TEXT="二阶非齐次" ID="NaoXEzGDvk" _mubu_text="%3Cspan%3E%E4%BA%8C%E9%98%B6%E9%9D%9E%E9%BD%90%E6%AC%A1%3C/span%3E" STYLE="fork">
+          <node TEXT="自由项f(x) = P_n(x)e^{ax}" ID="DyabU8GAvC" _mubu_text="%3Cspan%3E%E8%87%AA%E7%94%B1%E9%A1%B9%3C/span%3E%3Cspan%20class=%22%20formula%22%20data-raw=%22f(x)%2520%253D%2520P_n(x)e%255E%257Bax%257D%22%20contenteditable=%22false%22%3E%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%3C/span%3E" STYLE="fork">
+            <node TEXT="" ID="mGi6cvYVCb" _mubu_text="" STYLE="fork"/>
+          </node>
+          <node TEXT="自由项f(x) = e^{ax}[P_m(x)cosbx + P_n(x)sinbx]" ID="zfVmDFG35a" _mubu_text="%3Cspan%3E%E8%87%AA%E7%94%B1%E9%A1%B9%3C/span%3E%3Cspan%20class=%22%20formula%22%20data-raw=%22f(x)%2520%253D%2520e%255E%257Bax%257D%255BP_m(x)cosbx%2520%252B%2520P_n(x)sinbx%255D%22%20contenteditable=%22false%22%3E%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%3C/span%3E" STYLE="fork">
+            <node TEXT="" ID="AZsbmAAw3N" _mubu_text="" STYLE="fork"/>
+          </node>
+        </node>
+      </node>
     </node>
-    <node TEXT="欧拉方程" ID="2wTxy2Kry8" _mubu_text="%3Cspan%3E%E6%AC%A7%E6%8B%89%E6%96%B9%E7%A8%8B%3C/span%3E" STYLE="bubble" POSITION="right"/>
-    <node TEXT="" ID="2LERmXHXHY" _mubu_text="" STYLE="bubble" POSITION="left"/>
-    <node TEXT="" ID="JwV3r8wahp" _mubu_text="" STYLE="bubble" POSITION="left"/>
-    <node TEXT="" ID="i6kQoZ0p5M" _mubu_text="" STYLE="bubble" POSITION="left"/>
-    <node TEXT="" ID="rxg0w8UFzk" _mubu_text="" STYLE="bubble" POSITION="left"/>
+    <node TEXT="欧拉方程" ID="2wTxy2Kry8" _mubu_text="%3Cspan%20class=%22bold%22%3E%E6%AC%A7%E6%8B%89%E6%96%B9%E7%A8%8B%3C/span%3E" STYLE="bubble" POSITION="left">
+      <node TEXT="x^2y&apos;&apos; + pxy&apos; + qy = f(x) 变系数==&gt;常系数" ID="ELMMwYuXF9" _mubu_text="%3Cspan%20class=%22%20formula%22%20data-raw=%22x%255E2y&apos;&apos;%2520%252B%2520pxy&apos;%2520%252B%2520qy%2520%253D%2520f(x)%22%20contenteditable=%22false%22%3E%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%E2%80%8B%3C/span%3E%3Cspan%3E%20%E5%8F%98%E7%B3%BB%E6%95%B0==&amp;gt;%E5%B8%B8%E7%B3%BB%E6%95%B0%3C/span%3E" STYLE="fork">
+        <node TEXT="" ID="rIHgLAqGp3" _mubu_text="" STYLE="fork"/>
+      </node>
+    </node>
+    <node TEXT="例题" ID="rxg0w8UFzk" _mubu_text="%3Cspan%3E%E4%BE%8B%E9%A2%98%3C/span%3E" STYLE="bubble" POSITION="left">
+      <node TEXT="" ID="1nCySZZv1O" _mubu_text="" STYLE="fork"/>
+      <node TEXT="相消法" ID="e3KPHjbyWg" _mubu_text="%3Cspan%3E%E7%9B%B8%E6%B6%88%E6%B3%95%3C/span%3E" STYLE="fork"/>
+      <node TEXT="" ID="f9LEfyMuyb" _mubu_text="" STYLE="fork"/>
+    </node>
   </node>
 </map>
